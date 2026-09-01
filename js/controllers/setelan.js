b4e7b2e9a065cb3405643dfc075a710c1912eb0a
mrdokey <mrd.reborn33@gmail.com>
2026-09-01 09:44:46 +0800

    update fitur baru

--- js/controllers/setelan.js
+++ js/controllers/setelan.js
@@ -1,5 +1,5 @@
-import { db } from '../firebase.js';
-import { requestWAPairing, requestWAQr } from '../wa.js';
+import { db } from '../firebase.js?v=2';
+import { requestWAPairing, requestWAQr } from '../wa.js?v=2';
 
 let formConfig;
 const API_BASE = "https://wa.mrdsolution.my.id/api";
